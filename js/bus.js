e52ba2689dad7bd03df2ea1e7cf24873ca32d513
keyashiawillis@yahoo.com <>
2014-08-26 22:50:53 +0000

    CTAn

--- js/bus.js
+++ js/bus.js
@@ -1,33 +1,32 @@
-$.getJSPN('http://data.cityofchicago.org/resource/ag7u-gr9m.json', function (busstop){
-  $('#busstoplist').empty();
-  
-  $.each(lstops, function(i, busstop){
-    $('#busstoplist').append(generateLStopLink(busstop));
-    });
-    
-    $('#busstoplist').listview('refresh');
-    });
-    
-    function generateLStopLink(busstop){
-      return '<li><a href="javascript:void(0)'
-              +'"onclick="goToStationDetailPage(\''
-              +busstop.stop_name
-              +'\',\''
-              +busstop.station_descriptive_name + '\')">'
-              +busstop.stop_name
-              +'</a></li>'
-    }
-    
-    
-    function goToStopDetailPage(busstopName){
+$.getJSON('js/busstops.json', function(busstops){
+    $('#busstoplist').empty();
+
+    $.each(busstops, function(i,busstops){
+        $('#lstopslist').append( generateLStopLink(busstops) );
+});
+
+    $('#busstopslist').listview('refresh');
+});
+
+function generateLStopLink(busstop){
+
+    return '<li><a href="javascript:void(0)'
+        + '"onclick="goToStopDetailPage(\''
+        + busstop.stop_name
+        + '\',\''        
+        +busstop.station_decriptive_name +'\')">'
+        + busstop.stop_name
+        + '</a></li>';
+}
+function goToStopDetailPage(stopName){
     //create the page html template
-    var busstopPage = $("<div data-role= 'page' data-url=stop><div data-role='header'><h1>"
-                   + busstopName + "</h1></div><div data-role='content'></div><div data-role='footer'><h4>"
-                   + busstopName + "</h4></div></div>");
+var stopPage = $("<div data-role= 'page' data-url=station><div data-role='header'><h1>"
+        + stopName +"</h1></div><div data-role='content'></div><div data-role='footer'><h4>"
+        + stopName + "</h4></div></div>");
+   
+    //append the new page to the page container
+    stopPage.appendTo( $.mobile.pageContainer );
     
-    //append the new page to the page calculator
-    busstopPage.appendTo( $.mobile.PageContainer);
     //go to the newly created page
-    $.mobile.changePage( busstopPage);
-      
- }
+    $.mobile.changePage(stopPage);
+}
